24691b8b24c03860f96f2025f833ed9841d666cb
wenj91 <zlw420123@gmail.com>
2017-02-06 10:08:41 +0800

    LRU Cache java code

---
+++ src/main/java/LRUCache.java
@@ -0,0 +1,70 @@
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created by wenj91 on 2017-02-06.
+ */
+public class LRUCache<T> {
+
+    private int capacity; //equal 0 -> no capacity limit; great then 0 -> capacity limit
+    private final Map<String, T> cache;
+
+    public LRUCache(int capacity){
+        this.capacity = capacity;
+        this.cache = new LinkedHashMap<String, T>(capacity);
+    }
+
+    public LRUCache add(String key, T t){
+        synchronized (cache){
+            if(cache.get(key)!=null){
+                cache.remove(key);
+            }
+
+            cache.put(key, t);
+
+            if(capacity != 0 && cache.size()>capacity){
+                String firstKey = getFirstKey();
+                if(firstKey != null)
+                    cache.remove(firstKey);
+            }
+        }
+
+        return this;
+    }
+
+    public T get(String key){
+
+        T t = cache.get(key);
+
+        synchronized (cache) {
+            if (t != null) {
+                cache.remove(key);
+                cache.put(key, t);
+            }
+        }
+        return t;
+    }
+
+    public void del(String key){
+        synchronized (cache){
+            cache.remove(key);
+        }
+    }
+
+    public int getCacheSize(){
+        return cache.size();
+    }
+
+    private String getFirstKey(){
+        synchronized (cache) {
+            Iterator<String> it = cache.keySet().iterator();
+            String firstKey = null;
+            if(it.hasNext())
+                firstKey = it.next();
+
+            return firstKey;
+        }
+    }
+
+}
---
+++ src/test/java/LRUCacheTest.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by wenj91 on 2017-02-06.
+ */
+public class LRUCacheTest {
+
+    @Test
+    public void capacityTest(){
+        LRUCache<Integer> lruc = new LRUCache<>(0);
+        lruc.add("1", 1);
+        lruc.add("2", 2);
+        lruc.add("3", 3);
+        Assert.assertTrue(lruc.getCacheSize()==3);
+
+        lruc = new LRUCache<>(3);
+        lruc.add("1", 1);
+        lruc.add("2", 2);
+        lruc.add("3", 3);
+        lruc.add("4", 4);
+        Assert.assertTrue(lruc.getCacheSize()==3);
+    }
+}
